e8af1e1ece849d202a1c673cfda596ccbb0a9a04
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-04-15 23:23:36 +0200

    add sorting and displaying the result in js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import '../scss/index.scss';
+
+const input = document.querySelector(".sort__input");
+const output = document.querySelector(".sort__output");
+const btn = document.querySelector(".sort__btn");
+const descending = document.querySelector("#descending");
+
+const compare = (a, b) => {
+    return descending.checked ? b - a : a - b;
+};
+
+btn.addEventListener("click", () => {
+  const inputValue = input.value.split(",").sort(compare);
+  if(output.value) output.value = "";
+    
+  inputValue.forEach(e => {
+      const result = e.trim();
+      output.value += `${result}\n`;
+  });
+});
