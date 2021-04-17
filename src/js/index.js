790282ed6618fbd660714c62ad1e009f4fcca9d5
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-04-17 22:25:33 +0200

    add input validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,29 @@
-import '../scss/index.scss';
+import "../scss/index.scss";
 
 const input = document.querySelector(".sort__input");
 const output = document.querySelector(".sort__output");
 const btn = document.querySelector(".sort__btn");
 const descending = document.querySelector("#descending");
+const errMsg = document.querySelector(".error-message");
 
 const compare = (a, b) => {
-    return descending.checked ? b - a : a - b;
+  return descending.checked ? b - a : a - b;
 };
 
+const regexp = /^((\s*(0|([1-9][0-9]*)))\s*,\s*)+(0|([1-9][0-9]*))\s*$/;
+
 btn.addEventListener("click", () => {
-  const inputValue = input.value.split(",").sort(compare);
-  if(output.value) output.value = "";
-    
-  inputValue.forEach(e => {
+  if (regexp.test(input.value)) {
+    if(errMsg.style.opacity == 1) errMsg.style.opacity = 0;
+
+    const inputData = input.value.split(",").sort(compare);
+    if (output.value) output.value = "";
+
+    inputData.forEach((e) => {
       const result = e.trim();
       output.value += `${result}\n`;
-  });
+    });
+  } else {
+    errMsg.style.opacity = 1;
+  }
 });
